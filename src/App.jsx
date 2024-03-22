ea84ff3e568ee4939427fd19e2f91de2a3a1f1b6
axe-codes <dipesh.sedani@gmail.com>
2024-03-22 13:20:12 +0530

    UI Part done

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <center>
+      <div class="main-container">
+        <div class="sub-container">
+          <div className="sub">
+            <div class="inner-container sub-1 bg-image white-text a-items">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quibusdam fuga magnam voluptatem eaque laborum vel ex est iste
+              ullam quas sint libero dolorum, vero praesentium minima veritatis
+              fugiat earum facere?
+            </div>
+            <div class="inner-container sub-2 bg-image white-text a-items">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias
+              expedita quam rerum porro magni amet quae maiores, molestias et
+              hic reprehenderit fugit animi quod debitis iusto quis vel nihil
+              nisi!
+            </div>
+          </div>
+        </div>
+        <div class="sub-container bg-image"></div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
